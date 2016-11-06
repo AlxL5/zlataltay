5c1b14164beeb1dd7eec9522d2b519c7513cb969
Alexandr  Minenko <alexandr.minenko@gmail.com>
2016-11-06 16:13:31 +0700

    Вставил виджет вконтакте в шаблон

--- wp-content/themes/template/page.php
+++ wp-content/themes/template/page.php
@@ -17,4 +17,15 @@
         </section>
         <?php endwhile; endif; ?>
     </div><!-- #container-->
+
+    <div>
+        <script type="text/javascript" src="//vk.com/js/api/openapi.js?136"></script>
+
+        <!-- VK Widget -->
+        <div id="vk_groups"></div>
+        <script type="text/javascript">
+            VK.Widgets.Group("vk_groups", {mode: 3, width: "220"}, 20629724);
+        </script>
+    </div>
+
 <?php get_footer(); ?>
